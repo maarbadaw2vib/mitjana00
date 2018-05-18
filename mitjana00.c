eb32e654eb9428f1d466cc47f185101003661ef9
Marc <maarbadaw2vib@gmail.com>
2018-05-18 17:20:28 +0200

    Conflicte 1 entre master i br0 MARC

--- mitjana00.c
+++ mitjana00.c
@@ -21,6 +21,7 @@ int main() {
 		scanf(" %c",&consulta); 
 	}
 	mitjana=suma/numero_repeticions;
-	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("La mitjana resultant és: %.2f\n",mitjana);
+	printf("S'ha calculat la mitjana amb exit");	
 	return 0;
 }
